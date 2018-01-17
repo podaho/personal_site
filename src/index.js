0c9dfd5f9a280cc8b2ea042a43cced56f55fc3e5
Po <po.grammer.lin@gmail.com>
2018-01-17 12:22:48 -0600

    Responsive issues with Logo Design

--- src/index.js
+++ src/index.js
@@ -4,21 +4,41 @@
 
 const $ = require('jquery');
 
-const logoTopOffset = 100;
-const logoWidthHeight = 100;
+var logoTopOffset = 100;
+var logoWidthHeight = 100;
 const timeMultiplier = 400;
 var logoMovementTimeStep = 0;
 
 $(document).ready(() => {
-    console.log($(window).height());
+    logoTopOffset = $(window).height()*0.2;
+    logoWidthHeight = $(window).height()*0.2;
+
+    $("#logo-p").css({
+        "width": logoWidthHeight/2+"px",
+        "left": logoWidthHeight/4+"px",
+        "top": logoWidthHeight/4+"px"
+    });
+
+    $("#logo-l").css({
+        "width": logoWidthHeight/2+"px",
+        "left": logoWidthHeight/4+"px",
+        "top": logoWidthHeight/4+"px"
+    });
+
     $("#logo-p-container").css({
         "top": $(window).height()/2-logoTopOffset+"px",
-        "right": logoWidthHeight*(-1)+"px"
+        "right": logoWidthHeight*(-1)+"px",
+        "width": logoWidthHeight+"px",
+        "height": logoWidthHeight+"px",
+        "border-radius": logoWidthHeight/2+"px"
     });
 
     $("#logo-l-container").css({
         "top": $(window).height()/2-logoTopOffset+"px",
-        "left": logoWidthHeight*(-1)+"px"
+        "left": logoWidthHeight*(-1)+"px",
+        "width": logoWidthHeight+"px",
+        "height": logoWidthHeight+"px",
+        "border-radius": logoWidthHeight/2+"px"
     });
 
     var logoMovementIntervalId = setInterval(() => {
@@ -42,7 +62,35 @@ $(document).ready(() => {
     }, 1);
 
     $(window).resize(()=>{
-        $("#logo-p-container").css("top", $(window).height()/2-logoTopOffset+"px");
-        $("#logo-l-container").css("top", $(window).height()/2-logoTopOffset+"px");
+        const targetPixel = $(window).width()/2-logoWidthHeight/2;
+        logoTopOffset = $(window).height()*0.2;
+        logoWidthHeight = $(window).height()*0.2;
+
+        $("#logo-p").css({
+            "width": logoWidthHeight/2+"px",
+            "left": logoWidthHeight/4+"px",
+            "top": logoWidthHeight/4+"px"
+        });
+
+        $("#logo-l").css({
+            "width": logoWidthHeight/2+"px",
+            "left": logoWidthHeight/4+"px",
+            "top": logoWidthHeight/4+"px"
+        });
+
+        $("#logo-p-container").css({
+            "top": $(window).height()/2-logoTopOffset+"px",
+            "right": targetPixel,
+            "width": logoWidthHeight+"px",
+            "height": logoWidthHeight+"px",
+            "border-radius": logoWidthHeight/2+"px"
+        });
+        $("#logo-l-container").css({
+            "top": $(window).height()/2-logoTopOffset+"px",
+            "left": targetPixel,
+            "width": logoWidthHeight+"px",
+            "height": logoWidthHeight+"px",
+            "border-radius": logoWidthHeight/2+"px"
+        });
     });
 });
