a92185efea531cd2ab596cfd4d61b1a20bf076b3
podaho <po.grammer.lin@gmail.com>
2018-01-20 19:38:27 -0600

    Completed Navbar Functionality for Medium and Extra Large Screens

--- src/index.js
+++ src/index.js
@@ -4,93 +4,217 @@
 
 const $ = require('jquery');
 
-var logoTopOffset = 100;
-var logoWidthHeight = 100;
-const timeMultiplier = 400;
-var logoMovementTimeStep = 0;
-
 $(document).ready(() => {
-    logoTopOffset = $(window).height()*0.2;
-    logoWidthHeight = $(window).height()*0.2;
+    const logoP = $("#logo-p");
+    const logoL = $("#logo-l");
+    const logoPL = $("#logo-pl");
+    const logoContP = $("#logo-p-container");
+    const logoContL = $("#logo-l-container");
+    const logoContPL = $("#logo-container");
+    const navbar = $(".navbar-block");
+    const navbarText = $(".navbar-link");
+    const navbarTextEven = $(".navbar-link>span:nth-child(2n)");
+    const navbarTextOdd = $(".navbar-link>span:nth-child(2n+1)");
+    const pulseRing = $("#pulsing-ring");
+    const screenTopOffsetMultiplier = 0.05;
+    const screenLogoSizeMultiplier = 1/12;
+    var logoTopOffset = $(window).height()*screenTopOffsetMultiplier;
+    var logoWidthHeight = $(window).width()*screenLogoSizeMultiplier;
+    var logoMovementTimeStep = 0;
+    var pulseRingTimeStep = 0;
+    const timeMultiplier = 300;
+    var logoPulseHalt = true;
+    const logoFadeOut = () => {
+        if(!logoPulseHalt) {
+            logoContPL.fadeTo(1000, 0.4, logoFadeIn);
+        }
+    };
+    const logoFadeIn = () => {
+        if(!logoPulseHalt) {
+            logoContPL.fadeTo(1000, 1, logoFadeOut);
+        }
+    };
 
-    $("#logo-p").css({
+    logoP.css({
         "width": logoWidthHeight/2+"px",
         "left": logoWidthHeight/4+"px",
         "top": logoWidthHeight/4+"px"
     });
 
-    $("#logo-l").css({
+    logoL.css({
         "width": logoWidthHeight/2+"px",
         "left": logoWidthHeight/4+"px",
         "top": logoWidthHeight/4+"px"
     });
 
-    $("#logo-p-container").css({
-        "top": $(window).height()/2-logoTopOffset+"px",
+    logoPL.css({
+        "width": logoWidthHeight/2+"px",
+        "left": logoWidthHeight/4+"px",
+        "top": logoWidthHeight/4+"px"
+    });
+
+    logoContP.css({
+        "top": logoTopOffset+"px",
         "right": logoWidthHeight*(-1)+"px",
         "width": logoWidthHeight+"px",
         "height": logoWidthHeight+"px",
         "border-radius": logoWidthHeight/2+"px"
     });
 
-    $("#logo-l-container").css({
-        "top": $(window).height()/2-logoTopOffset+"px",
+    logoContL.css({
+        "top":logoTopOffset+"px",
         "left": logoWidthHeight*(-1)+"px",
         "width": logoWidthHeight+"px",
         "height": logoWidthHeight+"px",
         "border-radius": logoWidthHeight/2+"px"
     });
 
+    logoContPL.css({
+        "top": logoTopOffset+"px",
+        "left": $(window).width()/2-logoWidthHeight/2+"px",
+        "width": logoWidthHeight+"px",
+        "height": logoWidthHeight+"px",
+        "border-radius": logoWidthHeight/2+"px"
+    });
+
+    var logoPulsingIntervalId;
+    logoContPL.hover(() => {
+        logoPulseHalt = false;
+        logoFadeOut();
+    }, () => {
+        logoPulseHalt = true;
+        logoContPL.fadeTo(100, 1);
+    });
+
+    pulseRing.css({
+        "top": logoTopOffset+"px",
+        "left": $(window).width()/2-logoWidthHeight/2+"px",
+        "width": logoWidthHeight+"px",
+        "height": logoWidthHeight+"px",
+        "border-radius": logoWidthHeight/2+"px"
+    });
+
+    navbar.css({
+        "margin": logoTopOffset+logoWidthHeight*(1/3)+"px"
+    });
+
+    navbarText.css({
+        "font-size": logoWidthHeight*(1/5)+"px",
+        "visibility": "visible"
+    });
+
+    navbarTextEven.fadeTo(0, 0);
+    navbarTextOdd.fadeTo(0, 0);
+
     var logoMovementIntervalId = setInterval(() => {
         if(logoMovementTimeStep/timeMultiplier < 1) {
-            var height = $(window).height()/2-logoTopOffset+"px";
             var moveFuncValue = (-1)*Math.pow((logoMovementTimeStep/timeMultiplier-1),2)+1;
-            var targetPixel = $(window).width()/2-logoWidthHeight/2+logoWidthHeight;
-            $("#logo-p-container").css({
-                "right": moveFuncValue*targetPixel-logoWidthHeight+"px",
-                "top": height
+            var targetPixel = $(window).width()/2-logoWidthHeight/2;
+            logoContP.css({
+                "right": moveFuncValue*targetPixel+"px",
+                "top": logoTopOffset+"px"
             });
-        
-            $("#logo-l-container").css({
-                "left": moveFuncValue*targetPixel-logoWidthHeight+"px",
-                "top": height
+            logoContL.css({
+                "left": moveFuncValue*targetPixel+"px",
+                "top": logoTopOffset+"px"
             });
             logoMovementTimeStep++;
         } else {
             clearInterval(logoMovementIntervalId);
+            logoContP.remove();
+            logoContL.remove();
+            logoContPL.css("visibility", "visible");
+            pulseRing.css("visibility", "visible");
+            var newPulseRingWidthHeight = logoWidthHeight*(1+(pulseRingTimeStep/10));
+            var newPulseRingTopOffset = logoTopOffset-(newPulseRingWidthHeight-logoWidthHeight)/2;
+            var newPulseRingTargetPixel = ($(window).width()-newPulseRingWidthHeight)/2;
+            var pulseRingIntervalId = setInterval(() => {
+                if(pulseRingTimeStep < 90) {
+                    newPulseRingWidthHeight = logoWidthHeight*(1+(pulseRingTimeStep/10));
+                    newPulseRingTopOffset = logoTopOffset-(newPulseRingWidthHeight-logoWidthHeight)/2;
+                    newPulseRingTargetPixel = ($(window).width()-newPulseRingWidthHeight)/2;
+                    pulseRing.css({
+                        "width": newPulseRingWidthHeight+"px",
+                        "height": newPulseRingWidthHeight+"px",
+                        "top": newPulseRingTopOffset+"px",
+                        "left": newPulseRingTargetPixel+"px",
+                        "opacity": 1-pulseRingTimeStep/89
+                    });
+                    pulseRingTimeStep++;
+                } else {
+                    clearInterval(pulseRingIntervalId);
+                    pulseRing.remove();
+                }
+            }, 1);
+            navbarTextEven.fadeTo(800,1);
+            var evenCharTimeoutId = setTimeout(() => {
+                navbarTextOdd.fadeTo(800,1);
+            }, 400);
         }
     }, 1);
 
     $(window).resize(()=>{
-        const targetPixel = $(window).width()/2-logoWidthHeight/2;
-        logoTopOffset = $(window).height()*0.2;
-        logoWidthHeight = $(window).height()*0.2;
+        logoTopOffset = $(window).height()*screenTopOffsetMultiplier;
+        logoWidthHeight = $(window).width()*screenLogoSizeMultiplier;
+        var targetPixel = $(window).width()/2-logoWidthHeight/2;
+        console.log("width of window is: "+$(window).width()+"; logo width is: "+logoWidthHeight+"; centering pixel is: "+targetPixel);
 
-        $("#logo-p").css({
+        logoP.css({
             "width": logoWidthHeight/2+"px",
             "left": logoWidthHeight/4+"px",
             "top": logoWidthHeight/4+"px"
         });
 
-        $("#logo-l").css({
+        logoL.css({
             "width": logoWidthHeight/2+"px",
             "left": logoWidthHeight/4+"px",
             "top": logoWidthHeight/4+"px"
         });
 
-        $("#logo-p-container").css({
-            "top": $(window).height()/2-logoTopOffset+"px",
-            "right": targetPixel,
+        logoPL.css({
+            "width": logoWidthHeight/2+"px",
+            "left": logoWidthHeight/4+"px",
+            "top": logoWidthHeight/4+"px"
+        });
+
+        logoContP.css({
+            "top": logoTopOffset+"px",
+            "right": targetPixel+"px",
             "width": logoWidthHeight+"px",
             "height": logoWidthHeight+"px",
             "border-radius": logoWidthHeight/2+"px"
         });
-        $("#logo-l-container").css({
-            "top": $(window).height()/2-logoTopOffset+"px",
-            "left": targetPixel,
+
+        logoContL.css({
+            "top": logoTopOffset+"px",
+            "left": targetPixel+"px",
             "width": logoWidthHeight+"px",
             "height": logoWidthHeight+"px",
             "border-radius": logoWidthHeight/2+"px"
+        });
+
+        logoContPL.css({
+            "top": logoTopOffset+"px",
+            "left": targetPixel+"px",
+            "width": logoWidthHeight+"px",
+            "height": logoWidthHeight+"px",
+            "border-radius": logoWidthHeight/2+"px"
+        });
+
+        pulseRing.css({
+            "top": logoTopOffset+"px",
+            "left": targetPixel+"px",
+            "width": logoWidthHeight+"px",
+            "height": logoWidthHeight+"px",
+            "border-radius": logoWidthHeight/2+"px"
+        });
+
+        navbar.css({
+            "margin": logoTopOffset+logoWidthHeight*(1/3)+"px"
+        });
+
+        navbarText.css({
+            "font-size": logoWidthHeight*(1/5)+"px"
         });
     });
 });
